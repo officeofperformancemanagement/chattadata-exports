418c346846040adb85b82027a9b7ab93f3f52a8a
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-07-25 15:39:26 -0400

    Update dataset_list_generator.py

--- dataset_list_generator.py
+++ dataset_list_generator.py
@@ -4,9 +4,9 @@ import json
 f = open("./export_list.md", "w")
 
 # create table header
-table_header = "|four_by_four|dataset_name|dataset_link|" + "\n"
+table_header = "|four_by_four|dataset_name|dataset_link|duck_db|" + "\n"
 f.write(table_header)
-spacer = "| ----------- | ----------- |------------|"
+spacer = "| ----------- | ----------- |------------|-----------|"
 f.write(spacer)
 # get all the names from the files
 def extract_name_id_from_json(file_path):
@@ -23,7 +23,8 @@ def traverse_directory(directory_path):
                 file_path = os.path.join(root, file)
                 name, id = extract_name_id_from_json(file_path)
                 link = f"https://raw.githubusercontent.com/officeofperformancemanagement/chattadata-exports/main/data/{id}/{id}.csv.zip"
-                f.write (f"\n|{id}|{name}|[Link]({link})|")
+                duckdb_link=f"https://shell.duckdb.org/#queries=v0,CREATE-TABLE-dataset-AS-SELECT-*-FROM-'https%3A%2F%2Fwww.chattadata.org%2Fapi%2Fviews%2F{id.replace('-','%20')}%2Frows.csv%3FaccessType%3DDOWNLOAD'~,Describe-dataset~"
+                f.write (f"\n|{id}|{name}|[Link]({link})|[Link]({duckdb_link})|")
 directory_path = "./data"
 traverse_directory(directory_path)
 print("All good :)")
